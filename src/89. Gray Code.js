d46f55a7c9d67d813c4516684152359a8055c0da
Lilybon <zebra10029@gmail.com>
2020-06-10 16:08:42 +0800

    feat: add 89. Gray Code solution

---
+++ src/89. Gray Code.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+
+/*
+const grayCode = n => {
+  let arr = [0]
+  for (let i = 0; i < n; i++) {
+    for (let j = arr.length - 1; j >= 0; j--) {
+      arr.push(arr[j] | 1 << i)
+    }
+  }
+  return arr
+}
+ */
+
+const grayCode = (n) => {
+  if (n === 0) return [0]
+  const res = grayCode(n - 1)
+  let mask = 1 << (n - 1)
+  for (let i = res.length - 1; i >= 0; i--) {
+    res.push(res[i] | mask)
+  }
+  return res
+}
