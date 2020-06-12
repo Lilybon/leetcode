c91d9abaa14bc4745efcad36d5203fa5bebeb427
Lilybon <zebra10029@gmail.com>
2020-06-12 16:04:36 +0800

    feat: add 73. Set Matrix Zeroes solution

---
+++ src/73. Set Matrix Zeroes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+
+const setZeroes = (matrix) => {
+  let columnHasZero = {}
+  let rowHasZero = {}
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        columnHasZero[i] = true
+        rowHasZero[j] = true
+      }
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (columnHasZero[i] || rowHasZero[j]) {
+        matrix[i][j] = 0
+      }
+    }
+  }
+}
