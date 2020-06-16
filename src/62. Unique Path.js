29b74d50bce8aee368ed461c8174735fe5e5c369
Lilybon <zebra10029@gmail.com>
2020-06-16 18:01:56 +0800

    feat: add 62.Unique Paths solution

---
+++ src/62. Unique Path.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+
+/*
+const uniquePaths = (m, n) => {
+  if (!m || !n) return 1
+  let dp = []
+  for (let i = 0; i < m; i++) {
+    dp[i] = new Array(n)
+  }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === 0 || j === 0) dp[i][j] = 1
+      else dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    }
+  }
+  return dp[m - 1][n - 1]
+}
+*/
+
+const uniquePaths = (m, n) => {
+  let result = 1
+  for (let i = n; i <= m + n - 2; i++) {
+    result *= i
+    result /= i - n + 1
+  }
+  return result
+}
