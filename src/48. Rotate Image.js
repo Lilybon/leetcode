fca2c94376d00e94856511498bd908b4378ff99a
Lilybon <zebra10029@gmail.com>
2020-06-17 13:22:49 +0800

    feat: add 48. Rotate Image solution

---
+++ src/48. Rotate Image.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+
+const rotate = (matrix) => {
+  // imagine rotate one by one from outer row to center
+  const n = matrix.length
+  for (let i = 0; i < n / 2; i++) {
+    for (let j = i; j < n - i - 1; j++) {
+      let tmp = matrix[i][j]
+      matrix[i][j] = matrix[n - 1 - j][i]
+      matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+      matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+      matrix[j][n - 1 - i] = tmp
+    }
+  }
+}
