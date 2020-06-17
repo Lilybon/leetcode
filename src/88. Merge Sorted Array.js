ecf2275fb7a1e7652cd12a1bbca306b742cebc73
Lilybon <zebra10029@gmail.com>
2020-06-17 15:12:38 +0800

    feat: add 88. Merge Sorted Array solution

---
+++ src/88. Merge Sorted Array.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+/*
+ const merge = (nums1, m, nums2, n) => {
+  for (let i = m, j = 0; i < m + n; i++, j++) nums1[i]= nums2[j]
+  for (let i = 0; i < nums1.length - 1; i++) {
+    for (let j = i + 1; j < nums1.length; j++) {
+      if (nums1[i] > nums1[j]) [nums1[i], nums1[j]] = [nums1[j], nums1[i]]
+    }
+  }
+}
+ */
+
+const merge = (nums1, m, nums2, n) => {
+  let i = m - 1
+  let j = n - 1
+  let backward = m + n - 1
+  while (backward >= 0) {
+    if (j < 0 || nums1[i] > nums2[j]) {
+      nums1[backward] = nums1[i]
+      i--
+    } else if (i < 0 || nums1[i] <= nums2[j]) {
+      nums1[backward] = nums2[j]
+      j--
+    }
+    backward--
+  }
+}
