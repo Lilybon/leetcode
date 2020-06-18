ef06eab178109557d73d2a8cb0f171b504b107c8
Lilybon <zebra10029@gmail.com>
2020-06-18 14:04:31 +0800

    feat: add 347. Top K Frequent Elements solution

---
+++ src/347. Top K Frequent Elements.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+
+const topKFrequent = function (nums, k) {
+  const freq = nums.reduce((acc, val) => {
+    acc.set(val, acc.get(val) ? acc.get(val) + 1 : 1)
+    return acc
+  }, new Map())
+
+  return Array.from(freq)
+    .sort((a, b) => b[1] - a[1])
+    .map((pair) => pair[0])
+    .slice(0, k)
+}
