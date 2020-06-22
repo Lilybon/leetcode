fb22b45b1891f401c08b8f5f920b92d21852a426
Lilybon <zebra10029@gmail.com>
2020-06-22 13:39:54 +0800

    feat: add 152. Maximum Product Subarray solution

---
+++ src/152. Maximum Product Subarray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const maxProduct = (nums) => {
+  let max = 1,
+    min = 1,
+    res = -Number.MAX_VALUE
+  for (let val of nums) {
+    ;[max, min] = [
+      Math.max(val, max * val, min * val),
+      Math.min(val, max * val, min * val),
+    ]
+    res = Math.max(res, max)
+  }
+  return res
+}
