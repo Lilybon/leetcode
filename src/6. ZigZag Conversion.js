96377efc50b0d2cebf99ab7fb530c3c42bbca4bf
Lilybon <zebra10029@gmail.com>
2020-06-23 11:26:41 +0800

    feat: add 6. ZigZag Conversion solution

---
+++ src/6. ZigZag Conversion.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+const convert = (s, numRows) => {
+  if (numRows <= 1) return s
+  let zigzag = new Array(numRows).fill('')
+  let currRow = 0,
+    goDown = true
+  for (let char of s) {
+    zigzag[currRow] += char
+    if (goDown) {
+      if (currRow < numRows - 1) currRow++
+      else {
+        goDown = false
+        currRow--
+      }
+    } else {
+      if (currRow > 0) currRow--
+      else {
+        goDown = true
+        currRow++
+      }
+    }
+  }
+  return zigzag.join('')
+}
