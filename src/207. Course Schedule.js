591167d3c76604a4723de541f93b8c50e28e5b97
Lilybon <zebra10029@gmail.com>
2020-06-29 14:13:06 +0800

    feat: add 207. Course Schedule solution

---
+++ src/207. Course Schedule.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+
+function canFinish(numCourses, prerequisites) {
+  let visited = new Set()
+  let visiting = new Set()
+  let adj = [...Array(numCourses)].map((_) => [])
+
+  for (let [u, v] of prerequisites) {
+    adj[v].push(u)
+  }
+
+  for (let i = 0; i < numCourses; i++) {
+    if (hasCycle(i)) return false
+  }
+  return true
+
+  function hasCycle(v) {
+    if (visited.has(v)) return false
+    if (visiting.has(v)) return true
+    visiting.add(v)
+    for (let nv of adj[v]) {
+      if (hasCycle(nv)) return true
+    }
+    visiting.delete(v)
+    visited.add(v)
+    return false
+  }
+}
