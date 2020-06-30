c301ed3d6f67b0083afca074f8011b5e360729c1
Lilybon <zebra10029@gmail.com>
2020-06-30 16:01:42 +0800

    feat: add 581. Shortest Unsorted Continuous Subarray solution

---
+++ src/581. Shortest Unsorted Continuous Subarray.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+/*
+const findUnsortedSubarray = nums => {
+  const n = nums.length
+  const clone = [ ...nums ].sort((a, b) => a - b)
+  let i = 0,
+    j = n - 1
+  while (nums[i] === clone[i] && i < n) i++
+  while (nums[j] === clone[j] && j > i) j--
+  return j - i + 1
+}
+ */
+
+const findUnsortedSubarray = (nums) => {
+  const n = nums.length
+  let start = -1,
+    end = -2,
+    max = nums[0],
+    min = nums[n - 1]
+  for (let i = 0; i < n; i++) {
+    max = Math.max(max, nums[i])
+    min = Math.min(min, nums[n - 1 - i])
+    if (max > nums[i]) end = i
+    if (min < nums[n - 1 - i]) start = n - 1 - i
+  }
+  return end - start + 1
+}
