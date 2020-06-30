baf522cc04b5a961855d014e5daf3a86e6b6cfbe
Lilybon <zebra10029@gmail.com>
2020-06-30 11:06:23 +0800

    feat: add 55. Jump Game solution

---
+++ src/55. Jump Game.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+// DP Bottom-up
+/*
+const canJump = nums => {
+  const n = nums.length
+  let memo = new Array(n).fill(false)
+  memo[n - 1] = true
+  for (let i = n - 2; i >= 0; i--) {
+    let furtherJump = Math.min(i + nums[i], n - 1)
+    for (let j = i + 1; j <= furtherJump; j++) {
+      if (memo[j] === true) {
+        memo[i] = true
+        break
+      }
+    }
+  }
+  return memo[0] === true
+}
+ */
+
+// Greedy
+const canJump = (nums) => {
+  let lastPos = nums.length - 1
+  for (let i = lastPos; i >= 0; i--) {
+    if (i + nums[i] >= lastPos) lastPos = i
+  }
+  return lastPos === 0
+}
