4d0366b6a573df86e0ded47200e1e724c2db2cbe
Lilybon <zebra10029@gmail.com>
2020-06-30 15:01:15 +0800

    refactor: simplify 53. Maximum Subarray solution

--- src/53. Maximum Subarray.js
+++ src/53. Maximum Subarray.js
@@ -7,7 +7,7 @@ const maxSubArray = (nums) => {
   let curMax = 0,
     sumMax = -Number.MAX_VALUE
   for (let val of nums) {
-    curMax = Math.max(curMax + val, val)
+    curMax = val + Math.max(curMax, 0)
     sumMax = Math.max(sumMax, curMax)
   }
   return sumMax
