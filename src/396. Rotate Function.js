4d55db17b1980b97f6df2971382bdb500e1b233f
Lilybon <zebra10029@gmail.com>
2020-08-20 12:06:53 +0800

    feat: add 396. Rotate Function solution

---
+++ src/396. Rotate Function.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+
+const maxRotateFunction = (A) => {
+  const n = A.length
+  if (n === 0) return 0
+  let sum = 0
+  let f = 0
+  for (let i = 0; i < n; i++) {
+    sum += A[i]
+    f += A[i] * i
+  }
+  let max = f
+  for (let i = 1; i < n; i++) {
+    f += sum - n * A[n - i]
+    max = Math.max(max, f)
+  }
+  return max
+}
