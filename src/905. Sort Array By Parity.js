99add6798dca43ef054617680890f001f1783438
Lilybon <zebra10029@gmail.com>
2020-08-24 17:49:02 +0800

    feat: add 905. Sort Array By Parity solution

---
+++ src/905. Sort Array By Parity.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+
+const sortArrayByParity = (A) => {
+  let evens = []
+  let odds = []
+  for (let item of A) {
+    if (item % 2) odds.push(item)
+    else evens.push(item)
+  }
+  return evens.concat(odds)
+}
