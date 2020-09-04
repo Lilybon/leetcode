3e62c24e1113d6f73feb65548748c8fc6a2b78da
Lilybon <zebra10029@gmail.com>
2020-09-04 13:19:30 +0800

    feat: add 743. Network Delay Time solution

---
+++ src/743. Network Delay Time.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} times
+ * @param {number} N
+ * @param {number} K
+ * @return {number}
+ */
+
+// Bellman-Ford
+function networkDelayTime(times, N, K) {
+  const costs = Array(N + 1).fill(Infinity)
+  costs[0] = 0
+  costs[K] = 0
+  for (let i = 1; i < N; i++) {
+    let count = 0
+    for (let [u, v, t] of times) {
+      if (costs[u] === Infinity) continue
+      if (costs[v] > costs[u] + t) {
+        costs[v] = costs[u] + t
+        count++
+      }
+    }
+    if (!count) break
+  }
+  let max = Math.max(...costs)
+  return max === Infinity ? -1 : max
+}
