21cde924dd0dc6a577174525582012b20f5b43f7
Lilybon <zebra10029@gmail.com>
2020-09-11 01:32:54 +0800

    feat: add 118. Pascal's Triangle solution

---
+++ src/118. Pascal's Triangle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+
+const generate = (numRows) => {
+  let triangle = []
+  for (let i = 0; i < numRows; i++) {
+    let row = Array(i + 1).fill(1)
+    triangle.push(row)
+    for (let j = 1; j < i; j++) {
+      triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+    }
+  }
+  return triangle
+}
