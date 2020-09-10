09938ca6ec878d9117695e2ee9efba7fc803017c
Lilybon <zebra10029@gmail.com>
2020-09-11 00:33:26 +0800

    feat: add 70. Climbing Stairs solution

---
+++ src/70. Climbing Stairs.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+const climbStairs = (n) => {
+  let ones = 1
+  let twos = 0
+  for (let i = 1; i <= n - 1; i++) [ones, twos] = [ones + twos, ones]
+  return ones + twos
+}
