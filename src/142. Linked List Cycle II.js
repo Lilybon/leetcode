713a5f8d37c7c3461eba9c9f0f9d00ffa9c3a543
Lilybon <zebra10029@gmail.com>
2020-09-13 18:34:17 +0800

    feat: add 142. Linked List Cycle II solution

---
+++ src/142. Linked List Cycle II.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+const detectCycle = (head) => {
+  let slow = head,
+    fast = head
+  while (true) {
+    if (!fast || !fast.next) return null
+    slow = slow.next
+    fast = fast.next.next
+    if (slow === fast) break
+  }
+  slow = head
+  while (slow !== fast) {
+    slow = slow.next
+    fast = fast.next
+  }
+  return slow
+}
