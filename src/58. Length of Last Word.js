3a6d2347bc37bfb6c40138d95f8caadc3d2d4837
Lilybon <zebra10029@gmail.com>
2020-09-16 20:42:14 +0800

    feat: add 58. Length of Last Word solution

---
+++ src/58. Length of Last Word.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+const lengthOfLastWord = (s) => {
+  let n = 0
+  for (let j = s.length - 1; j >= 0; j--) {
+    if (s[j] !== ' ') n++
+    else if (s[j] === ' ' && n) break
+  }
+  return n
+}
