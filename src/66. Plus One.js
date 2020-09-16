df8095b7ff2290a12d6c2691e54d15a043b0f83f
Lilybon <zebra10029@gmail.com>
2020-09-16 21:14:02 +0800

    feat: add 66. Plus One solution

---
+++ src/66. Plus One.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+const plusOne = (digits) => {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] === 9) digits[i] = 0
+    else {
+      digits[i] += 1
+      break
+    }
+  }
+  if (digits[0] === 0) digits.unshift(1)
+  return digits
+}
