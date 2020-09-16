c7750babcb68f4fc59c59c3d3ab4170981dcda1b
Lilybon <zebra10029@gmail.com>
2020-09-16 20:06:10 +0800

    feat: add 398. Random Pick Index solution

---
+++ src/398. Random Pick Index.js
@@ -0,0 +1,30 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   */
+  constructor(nums) {
+    this.nums = nums
+  }
+  /**
+   * @param {number} target
+   * @return {number}
+   */
+  pick(target) {
+    let result = -1
+    let count = 0
+    for (let idx in this.nums) {
+      if (this.nums[idx] === target) {
+        count++
+        let fit = Math.floor(Math.random() * count)
+        if (fit === 0) result = idx
+      }
+    }
+    return result
+  }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.pick(target)
+ */
