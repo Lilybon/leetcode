af5acae534b260aa14662d200fe6dc083dd4c8b0
Lilybon <zebra10029@gmail.com>
2020-09-18 18:13:30 +0800

    feat: add 204. Count Primes solution

---
+++ src/204. Count Primes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+const countPrimes = (n) => {
+  if (n < 3) return 0
+  let f = new Uint8Array(n).fill(1)
+  f[0] = 0
+  f[1] = 0
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (f[i] === 0) continue
+    for (let j = i * i; j < n; j += i) f[j] = 0
+  }
+  return f.reduce((sum, count) => sum + count, 0)
+}
