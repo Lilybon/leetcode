01da39ee0b45aced852336ed5184d30ac97edbac
Lilybon <zebra10029@gmail.com>
2020-09-18 15:11:02 +0800

    feat: add 290. Word Pattern solution

---
+++ src/290. Word Pattern.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+
+/*
+const wordPattern = (pattern, s) => {
+  let wordList = s.split(' ')
+  const n = pattern.length
+  if (n !==  wordList.length) return false
+  const ptw = Array(26)
+  for (let i = 0; i < n; i++) {
+    let p = pattern[i]
+    let w = wordList[i]
+    let j = p.charCodeAt(0) - 97
+    if (!ptw[j]) {
+      if (ptw.includes(w)) return false
+      ptw[j] = w
+    }
+    if (ptw[j] !== w) return false
+  }
+  return true
+}
+ */
+
+const wordPattern = (pattern, s) => {
+  let wordList = s.split(' ')
+  const n = pattern.length
+  if (n !== wordList.length) return false
+  const ptw = new Map()
+  const wtp = new Map()
+  for (let i = 0; i < n; i++) {
+    let p = pattern[i]
+    let w = wordList[i]
+    if (!ptw.has(p) && !wtp.get(w)) {
+      ptw.set(p, w)
+      wtp.set(w, p)
+    }
+    if (ptw.get(p) !== w || wtp.get(w) !== p) return false
+  }
+  return true
+}
