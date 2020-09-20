807664ff6900884ff7491f167d4b4bf58e040ca6
Lilybon <zebra10029@gmail.com>
2020-09-20 20:13:16 +0800

    feat: add 79. Word Search solution

---
+++ src/79. Word Search.js
@@ -0,0 +1,70 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+
+/*
+function exist (board, word) {
+  if (!board.length || !board[0].length) return false
+  const m = board.length
+  const n = board[0].length
+  const visited = Array(m).fill().map(_ => Array(n))
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (search(i, j, 0)) return true
+    }
+  }
+  return false
+  function search (x, y, idx) {
+    if (idx >= word.length) return true
+    if (x < 0 ||
+        x >= m ||
+        y < 0 ||
+        y >= n ||
+        visited[x][y] ||
+        board[x][y] !== word[idx]) return false
+    visited[x][y] = true
+    let found = search(x + 1, y, idx + 1) ||
+       search(x - 1, y, idx + 1) ||
+       search(x, y + 1, idx + 1) ||
+       search(x, y - 1, idx + 1)
+    visited[x][y] = false
+    return found
+  }
+}
+ */
+
+// reset original board grid value when backtracking
+function exist(board, word) {
+  if (!board.length || !board[0].length) return false
+  const m = board.length
+  const n = board[0].length
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (search(i, j, 0)) return true
+    }
+  }
+  return false
+  function search(x, y, idx) {
+    if (idx >= word.length) return true
+    if (
+      x < 0 ||
+      x >= m ||
+      y < 0 ||
+      y >= n ||
+      board[x][y] === '@@' ||
+      board[x][y] !== word[idx]
+    )
+      return false
+    let backup = board[x][y]
+    board[x][y] = '@@'
+    let found =
+      search(x + 1, y, idx + 1) ||
+      search(x - 1, y, idx + 1) ||
+      search(x, y + 1, idx + 1) ||
+      search(x, y - 1, idx + 1)
+    board[x][y] = backup
+    return found
+  }
+}
