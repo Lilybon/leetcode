9cdb6630c570d5c0922827a6e017b884ce16ff09
Lilybon <zebra10029@gmail.com>
2020-09-21 23:11:59 +0800

    feat: add 46. Permutations solution

---
+++ src/46. Permutations.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+function permute(nums) {
+  const result = new Array()
+  const visited = new Uint8Array(nums.length)
+  const row = new Array()
+  dfs()
+  return result
+  function dfs() {
+    if (row.length === nums.length) {
+      result.push([...row])
+      return
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (visited[i]) continue
+      visited[i] = 1
+      row.push(nums[i])
+      dfs()
+      visited[i] = 0
+      row.pop()
+    }
+  }
+}
