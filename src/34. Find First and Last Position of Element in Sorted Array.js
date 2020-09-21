b6c0995728068eded03cadebeefd9d5734791ddf
Lilybon <zebra10029@gmail.com>
2020-09-21 22:34:00 +0800

    feat: add 34. Find First and Last Position of Element in Sorted Array solution

---
+++ src/34. Find First and Last Position of Element in Sorted Array.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+function searchRange(nums, target) {
+  return [searchFirst(nums, target), searchLast(nums, target)]
+}
+
+function searchFirst(nums, target) {
+  let left = 0,
+    right = nums.length
+  while (left < right) {
+    let mid = Math.floor((left + right - 1) / 2)
+    if (nums[mid] >= target) right = mid
+    else left = mid + 1
+  }
+  if (left === nums.length || nums[left] !== target) return -1
+  return left
+}
+
+function searchLast(nums, target) {
+  let left = 0,
+    right = nums.length
+  while (left < right) {
+    let mid = Math.floor((left + right - 1) / 2)
+    if (nums[mid] > target) right = mid
+    else left = mid + 1
+  }
+  left--
+  if (left < 0 || nums[left] !== target) return -1
+  return left
+}
