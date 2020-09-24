5bc09b736fea01d48b5fa28a99ba04f70bb78de6
Lilybon <zebra10029@gmail.com>
2020-09-24 10:14:02 +0800

    feat: add 1161. Maximum Level Sum of a Binary Tree solution

---
+++ src/1161. Maximum Level Sum of a Binary Tree.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+const maxLevelSum = (root) => {
+  let queue = [],
+    level = 1,
+    result = 1,
+    globalMax = 0
+  queue.push([root])
+  while (queue.length) {
+    let nodeList = queue.shift(),
+      localMax = 0,
+      nextList = []
+    for (let node of nodeList) {
+      localMax += node.val
+      if (node.left) nextList.push(node.left)
+      if (node.right) nextList.push(node.right)
+    }
+    if (localMax > globalMax) {
+      result = level
+      globalMax = localMax
+    }
+    level++
+    if (nextList.length) queue.push(nextList)
+  }
+  return result
+}
