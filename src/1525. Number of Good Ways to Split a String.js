68a781238a794db9003651e1ef441f1406887d2e
Lilybon <zebra10029@gmail.com>
2020-09-24 09:53:25 +0800

    feat: add 1525. Number of Good Ways to Split a String solution

---
+++ src/1525. Number of Good Ways to Split a String.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+const numSplits = (s) => {
+  let count = 0,
+    leftMap = new Map(),
+    rightMap = new Map(),
+    leftUnique = 0,
+    rightUnique = 0
+  for (let char of s) {
+    if (!rightMap.has(char)) rightUnique++
+    rightMap.set(char, rightMap.has(char) ? rightMap.get(char) + 1 : 1)
+  }
+  for (let char of s) {
+    if (!leftMap.has(char)) leftUnique++
+    leftMap.set(char, leftMap.has(char) ? leftMap.get(char) + 1 : 1)
+    rightMap.set(char, rightMap.get(char) - 1)
+    if (rightMap.get(char) === 0) rightUnique--
+    if (leftUnique === rightUnique) count++
+  }
+  return count
+}
