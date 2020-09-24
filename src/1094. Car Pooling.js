80755cc94c6e2e3eb231969fefe560acb8878287
Lilybon <zebra10029@gmail.com>
2020-09-24 18:10:22 +0800

    feat: add 1094. Car Pooling solution

---
+++ src/1094. Car Pooling.js
@@ -0,0 +1,13 @@
+const carPooling = (trips, capacity) => {
+  let MAX_STOP = 1000
+  let stops = Array(MAX_STOP).fill(0)
+  for (let i = 0; i < trips.length; i++) {
+    stops[trips[i][1]] += trips[i][0]
+    stops[trips[i][2]] -= trips[i][0]
+  }
+  for (let i = 1; i < MAX_STOP; i++) {
+    stops[i] += stops[i - 1]
+    if (stops[i] > capacity) return false
+  }
+  return true
+}
