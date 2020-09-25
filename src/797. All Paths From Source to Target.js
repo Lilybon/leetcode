bd57e34b04a08baaa10797f97943faf54ec244bf
Lilybon <zebra10029@gmail.com>
2020-09-25 16:40:41 +0800

    feat: add 797. All Paths From Source to Target solution

---
+++ src/797. All Paths From Source to Target.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+
+function allPathsSourceTarget(graph) {
+  const result = [],
+    path = [0],
+    n = graph.length
+  dfs(0)
+  return result
+  function dfs(node) {
+    if (node === n - 1) {
+      result.push([...path])
+      return
+    }
+    for (let next of graph[node]) {
+      path.push(next)
+      dfs(next)
+      path.pop()
+    }
+  }
+}
