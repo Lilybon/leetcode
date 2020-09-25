880919633736414966b5174de7b765bbaab8757c
Lilybon <zebra10029@gmail.com>
2020-09-25 10:45:35 +0800

    feat: add 77. Combinations solution

---
+++ src/77. Combinations.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+
+function combine(n, k) {
+  const result = new Array()
+  const visited = new Uint8Array(n + 1)
+  const row = new Array()
+  dfs()
+  return result
+  function dfs(start = 1) {
+    if (row.length === k) {
+      result.push([...row])
+      return
+    }
+    for (let i = start; i <= n; i++) {
+      if (visited[i]) continue
+      visited[i] = 1
+      row.push(i)
+      dfs(i + 1)
+      visited[i] = 0
+      row.pop(i)
+    }
+  }
+}
