f0cb81a811aebf99562ce4138058c8c8741f8f23
Lilybon <zebra10029@gmail.com>
2020-09-26 17:44:06 +0800

    feat: add 977. Squares of a Sorted Array solution

---
+++ src/977. Squares of a Sorted Array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+
+const sortedSquares = (A) => {
+  const result = Array(A.length)
+  let left = 0,
+    right = A.length - 1,
+    index = A.length - 1
+  while (left <= right) {
+    let leftSqrt = A[left] ** 2
+    let rightSqrt = A[right] ** 2
+    if (leftSqrt >= rightSqrt) {
+      result[index--] = leftSqrt
+      left++
+    } else {
+      result[index--] = rightSqrt
+      right--
+    }
+  }
+  return result
+}
