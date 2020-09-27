4c58b8f0f430282f27a0fd25a597f750528d576b
Lilybon <zebra10029@gmail.com>
2020-09-27 17:09:04 +0800

    feat: add 713. Subarray Product Less Than K solution

---
+++ src/713. Subarray Product Less Than K.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+const numSubarrayProductLessThanK = (nums, k) => {
+  let count = 0,
+    prod = 1,
+    left = 0
+  for (let right in nums) {
+    prod *= nums[right]
+    while (left <= right && prod >= k) prod /= nums[left++]
+    count += right - left + 1
+  }
+  return count
+}
