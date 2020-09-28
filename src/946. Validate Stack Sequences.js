c82fe94a5cb3da69844299eb5d6014ac5a9dde81
Lilybon <zebra10029@gmail.com>
2020-09-28 17:49:01 +0800

    feat: add 946. Validate Stack Sequences solution

---
+++ src/946. Validate Stack Sequences.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} pushed
+ * @param {number[]} popped
+ * @return {boolean}
+ */
+
+const validateStackSequences = (pushed, popped) => {
+  let j = 0,
+    stack = []
+  for (let i = 0; i < pushed.length; i++) {
+    stack.push(pushed[i])
+    while (stack.length && stack[stack.length - 1] === popped[j]) {
+      stack.pop()
+      j++
+    }
+  }
+  return j === pushed.length
+}
