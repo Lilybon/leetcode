53b9cd924879728c0f33779561a83399aa2d5c52
Lilybon <zebra10029@gmail.com>
2020-09-29 17:19:12 +0800

    feat: add 648. Replace Words solution

---
+++ src/648. Replace Words.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
+
+function replaceWords(dictionary, sentence) {
+  const trie = dictionary.reduce((map, word) => buildMap(map, word), {})
+  return sentence
+    .split(' ')
+    .map((word) => replaceWord(trie, word))
+    .join(' ')
+}
+
+function buildMap(map, word) {
+  let node = map,
+    idx = 0
+  while (idx < word.length) {
+    if (node.isEnd) return map
+    let char = word[idx]
+    if (!node[char]) node[char] = {}
+    node = node[char]
+    idx++
+  }
+  node.isEnd = true
+  return map
+}
+
+function replaceWord(map, word) {
+  let node = map,
+    idx = 0,
+    shorten = ''
+  while (idx < word.length) {
+    let char = word[idx]
+    if (node.isEnd) return shorten
+    if (!node[char]) return word
+    shorten += char
+    node = node[char]
+    idx++
+  }
+  return node.isEnd ? shorten : word
+}
