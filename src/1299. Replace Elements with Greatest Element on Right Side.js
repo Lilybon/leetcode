553a8686954c87f3d970bcd7550e21be9900b246
Lilybon <zebra10029@gmail.com>
2020-09-30 16:25:25 +0800

    feat: add 1299. Replace Elements with Greatest Element on Right Side solution

---
+++ src/1299. Replace Elements with Greatest Element on Right Side.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+
+const replaceElements = (arr) => {
+  const n = arr.length
+  const result = Array(n)
+  result[n - 1] = -1
+  let max = -Infinity
+  for (let i = n - 2; i >= 0; i--) {
+    max = Math.max(max, arr[i + 1])
+    result[i] = max
+  }
+  return result
+}
