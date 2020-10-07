cfb556ee48499906d3e11d4778fd2f2cbc8199f3
Lilybon <zebra10029@gmail.com>
2020-10-07 17:23:27 +0800

    feat: add 950. Reveal Cards In Increasing Order solution

---
+++ src/950. Reveal Cards In Increasing Order.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} deck
+ * @return {number[]}
+ */
+
+const deckRevealedIncreasing = (deck) => {
+  const n = deck.length,
+    result = new Uint32Array(n),
+    queue = Array.from(Array(n), (_, idx) => n - 1 - idx)
+  deck.sort((a, b) => a - b)
+  for (let val of deck) {
+    let front = queue.pop()
+    result[front] = val
+    let next = queue.pop()
+    queue.unshift(next)
+  }
+  return result
+}
