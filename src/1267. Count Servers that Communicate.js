0bf9a7825474d49a205748882d67ed151f74e2ab
Lilybon <zebra10029@gmail.com>
2020-10-08 21:56:57 +0800

    feat: add 1267. Count Servers that Communicate solution

---
+++ src/1267. Count Servers that Communicate.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+
+const countServers = (grid) => {
+  let count = 0
+  const rowCounts = new Uint32Array(grid.length),
+    colCounts = new Uint32Array(grid[0].length)
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j]) {
+        rowCounts[i]++
+        colCounts[j]++
+      }
+    }
+  }
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] && (rowCounts[i] > 1 || colCounts[j] > 1)) count++
+    }
+  }
+  return count
+}
