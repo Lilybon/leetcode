567566ecba0260ed3b3de3369167f275e320948d
Lilybon <zebra10029@gmail.com>
2020-10-09 18:54:13 +0800

    feat: add 695. Max Area of Island solution

---
+++ src/695. Max Area of Island.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+
+function maxAreaOfIsland(grid) {
+  let max = 0,
+    tmpMax = 0
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j]) {
+        tmpMax = 0
+        countAndMark(i, j)
+        max = Math.max(max, tmpMax)
+      }
+    }
+  }
+  return max
+  function countAndMark(i, j) {
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      j < 0 ||
+      j >= grid[0].length ||
+      !grid[i][j]
+    )
+      return
+    grid[i][j] = 0
+    tmpMax++
+    countAndMark(i - 1, j)
+    countAndMark(i + 1, j)
+    countAndMark(i, j - 1)
+    countAndMark(i, j + 1)
+  }
+}
