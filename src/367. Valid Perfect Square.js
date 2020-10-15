4b6580e1347afef7f29a4f06f04fad2488d8737a
Lilybon <zebra10029@gmail.com>
2020-10-15 22:04:28 +0800

    refactor: update 367. Valid Perfect Square solution

--- src/367. Valid Perfect Square.js
+++ src/367. Valid Perfect Square.js
@@ -3,7 +3,16 @@
  * @return {boolean}
  */
 
+/*
 export function isPerfectSquare(num) {
   const sqrtNum = Math.sqrt(num)
   return sqrtNum === parseInt(sqrtNum)
+}
+ */
+
+export function isPerfectSquare (nums) {
+  for (let i = 1; i <= nums / i; i++) {
+    if (i ** 2 === nums) return true
+  }
+  return false
 }
