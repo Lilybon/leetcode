2288201049e387d0ec77febc3ffd00b3b1926210
Lilybon <zebra10029@gmail.com>
2020-12-09 18:13:55 +0800

    refactor: optimize 73. Set Matrix Zeroes solution

--- src/73. Set Matrix Zeroes.js
+++ src/73. Set Matrix Zeroes.js
@@ -3,6 +3,8 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 
+// O(m + n)space
+/*
 export function setZeroes(matrix) {
   let columnHasZero = {}
   let rowHasZero = {}
@@ -19,6 +21,40 @@ export function setZeroes(matrix) {
       if (columnHasZero[i] || rowHasZero[j]) {
         matrix[i][j] = 0
       }
+    }
+  }
+}
+ */
+
+// O(1)space - store map value on first row and first column
+export function setZeroes(matrix) {
+  const R = matrix.length,
+    C = matrix[0].length
+  let firstColHasZero = false
+  for (let i = 0; i < R; i++) {
+    if (!matrix[i][0]) firstColHasZero = true
+    for (let j = 1; j < C; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0
+        matrix[0][j] = 0
+      }
+    }
+  }
+  for (let i = 1; i < R; i++) {
+    for (let j = 1; j < C; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0
+      }
+    }
+  }
+  if (matrix[0][0] === 0) {
+    for (let i = 0; i < C; i++) {
+      matrix[0][i] = 0
+    }
+  }
+  if (firstColHasZero) {
+    for (let i = 0; i < R; i++) {
+      matrix[i][0] = 0
     }
   }
 }
