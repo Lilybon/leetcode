39258c28a9468706c8a0c1c9124bf00b82599797
Lilybon <zebra10029@gmail.com>
2020-12-21 18:18:54 +0800

    refactor: simplify dfs logic

--- src/841. Keys and Rooms.js
+++ src/841. Keys and Rooms.js
@@ -5,14 +5,13 @@
 
 export function canVisitAllRooms(rooms) {
   const visited = Array(rooms.length).fill(false)
-  visited[0] = true
   dfs(0)
   return visited.every((room) => room)
   function dfs(idx) {
+    if (visited[idx]) return
+    visited[idx] = true
     const nextRooms = rooms[idx]
     for (let nextRoom of nextRooms) {
-      if (visited[nextRoom]) continue
-      visited[nextRoom] = true
       dfs(nextRoom)
     }
   }
