0c74de0884b0261ab05ec1775a340c7d0793d665
Lilybon <zebra10029@gmail.com>
2021-03-30 19:56:50 +0800

    refactor: optimize 63. Unique Paths II solution

--- src/63. Unique Paths II.js
+++ src/63. Unique Paths II.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 
+/*
 export function uniquePathsWithObstacles(obstacleGrid) {
   if (obstacleGrid[0][0]) return 0
   const m = obstacleGrid.length,
@@ -23,4 +24,22 @@ export function uniquePathsWithObstacles(obstacleGrid) {
     }
   }
   return dp[m - 1][n - 1]
+}
+ */
+
+export function uniquePathsWithObstacles(obstacleGrid) {
+  if (obstacleGrid[0][0]) return 0
+  const m = obstacleGrid.length,
+    n = obstacleGrid[0].length,
+    dp = Array(n).fill(0)
+  for (let i = 0; i < n; i++) {
+    if (obstacleGrid[0][i]) break
+    dp[i] = 1
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      dp[j] = obstacleGrid[i][j] ? 0 : (dp[j - 1] || 0) + dp[j]
+    }
+  }
+  return dp[n - 1]
 }
