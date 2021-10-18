65b658cf67cbd6dc9870f829e1e025bb8c8741aa
Lilybon <zebra10029@gmail.com>
2021-10-18 22:04:36 +0800

    feat: add 95. Unique Binary Search Trees II solution

---
+++ src/95. Unique Binary Search Trees II.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+
+function generateTrees(n) {
+  /**
+   * Assuming Tree(n) returns all possible BST combinations,
+   * we can infer that
+   * Tree(n) =
+   * Tree(1) * Tree(n - 1) +
+   * Tree(2) * Tree(n - 2) +
+   * ... +
+   * Tree(n - 1) * Tree(1)
+   */
+  return n === 0 ? [] : collectTrees(1, n)
+  function collectTrees(left, right) {
+    if (left > right) return [null]
+    const results = []
+    for (let i = left; i <= right; i++) {
+      const leftTrees = collectTrees(left, i - 1)
+      const rightTrees = collectTrees(i + 1, right)
+      for (let leftTree of leftTrees) {
+        for (let rightTree of rightTrees) {
+          const tree = new TreeNode(i, leftTree, rightTree)
+          results.push(tree)
+        }
+      }
+    }
+    return results
+  }
+}
