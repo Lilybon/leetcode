c5aad3922b21325aaef9e97dbabe6cd9e9bfe0de
Lilybon <zebra10029@gmail.com>
2021-11-23 22:53:34 +0800

    refactor: update 101. Symmetric Tree solution

--- src/101. Symmetric Tree.js
+++ src/101. Symmetric Tree.js
@@ -6,33 +6,22 @@
  * }
  */
 
-function revertTree(node) {
-  if (node == null || (node.left == null && node.right == null)) {
-    return node
-  }
-  let tmp = revertTree(node.left)
-  node.left = revertTree(node.right)
-  node.right = tmp
-  return node
-}
-
-function isSameTree(left, right) {
-  if (left == null && right == null) return true
-  if ((left == null && right != null) || (left != null && right == null))
-    return false
-  if (left.val != right.val) return false
-  return (
-    isSameTree(left.left, right.left) && isSameTree(left.right, right.right)
-  )
-}
-
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
 
 function isSymmetric(root) {
-  if (root == null || (root.left == null && root.right == null)) return true
-  root.right = revertTree(root.right)
-  return isSameTree(root.left, root.right)
+  if (root === null) return true
+  return isSameNode(root.left, root.right)
+}
+
+function isSameNode(left, right) {
+  if (left === null && right === null) return true
+  if (left !== null && right === null) return false
+  if (left === null && right !== null) return false
+  if (left.val !== right.val) return false
+  return (
+    isSameNode(left.right, right.left) && isSameNode(left.left, right.right)
+  )
 }
