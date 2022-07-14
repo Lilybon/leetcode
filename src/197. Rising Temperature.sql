b9c522c9a1aefb4e39e237446c1faac4c283c208
Lilybon <zebra10029@gmail.com>
2022-07-14 20:50:00 +0800

    feat: add 197. Rising Temperature solution

---
+++ src/197. Rising Temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT w1.Id
+FROM   Weather w1,
+       Weather w2
+WHERE  w1.Temperature > w2.Temperature
+       AND TO_DAYS(w1.RecordDate) = TO_DAYS(w2.RecordDate) + 1; 
