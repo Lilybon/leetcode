1c38449e497556fa04d04dd713595418aae1a71e
Lilybon <zebra10029@gmail.com>
2022-07-18 19:11:16 +0800

    feat: add 1581. Customer Who Visited but Did Not Make Any Transactions solution

---
+++ src/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT customer_id,
+       Count(visit_id) AS count_no_trans
+FROM   visits
+WHERE  visit_id NOT IN (SELECT DISTINCT visit_id
+                        FROM   transactions)
+GROUP  BY customer_id
