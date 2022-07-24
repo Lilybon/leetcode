57084df4b25602177404e3302dcd6ad02d99b296
Lilybon <zebra10029@gmail.com>
2022-07-24 09:08:48 +0800

    refactor: update 240. Search a 2D Matrix II solution

--- src/240. Search a 2D Matrix II.js
+++ src/240. Search a 2D Matrix II.js
@@ -5,15 +5,12 @@
  */
 
 export function searchMatrix(matrix, target) {
-  const m = matrix.length
-  if (!m) return false
-  const n = matrix[0].length
-  if (!n) return false
+  const m = matrix.length,
+    n = matrix[0].length
   let i = m - 1,
     j = 0
-  while (true) {
-    if (i < 0 || j >= n) break
-    else if (matrix[i][j] > target) i--
+  while (i >= 0 && j < n) {
+    if (matrix[i][j] > target) i--
     else if (matrix[i][j] < target) j++
     else return true
   }
