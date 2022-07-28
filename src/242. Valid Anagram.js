e1f9bd827dd829b936832f91f34447c0dffaceb5
Lilybon <zebra10029@gmail.com>
2022-07-28 10:16:59 +0800

    refactor: update 242. Valid Anagram solution

--- src/242. Valid Anagram.js
+++ src/242. Valid Anagram.js
@@ -4,10 +4,27 @@
  * @return {boolean}
  */
 
+// function sortStr (str) {
+//   return str.split('').sort().join('')
+// }
+
+// function isAnagram(s, t) {
+//   if (s.length != t.length) return false
+//   const sortS = sortStr(s)
+//   const sortT = sortStr(t)
+//   return sortS === sortT
+// }
+
 export function isAnagram(s, t) {
-  const sortStr = (str) => str.split('').sort().join('')
-  if (s.length != t.length) return false
-  const sortS = sortStr(s)
-  const sortT = sortStr(t)
-  return sortS === sortT
+  if (s.length !== t.length) return false
+  const map = new Map()
+  for (let char of s) {
+    map.set(char, (map.get(char) ?? 0) + 1)
+  }
+  for (let char of t) {
+    const count = map.get(char)
+    if (!count) return false
+    map.set(char, count - 1)
+  }
+  return true
 }
