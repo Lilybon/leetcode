d9209f32083cf92b32a9325b127a447d8c1ce282
Lilybon <zebra10029@gmail.com>
2022-07-30 12:39:11 +0800

    fix: correct 746. Min Cost Climbing Stairs assertion

--- test/746. Min Cost Climbing Stairs.test.js
+++ test/746. Min Cost Climbing Stairs.test.js
@@ -1,7 +1,7 @@
 import { minCostClimbingStairs } from '../src/746. Min Cost Climbing Stairs'
 
-describe('it finds minimum cost to reach the top of the floor', () => {
-  test('', () => {
+describe('minCostClimbingStairs', () => {
+  test('it finds minimum cost to reach the top of the floor', () => {
     expect(minCostClimbingStairs([10, 15, 20])).toBe(15)
     expect(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])).toBe(6)
   })
