8a395ef7d62e97a039e77fd78c237022066b0c62
Lilybon <zebra10029@gmail.com>
2022-08-01 19:07:08 +0800

    feat: add 2304. Minimum Path Cost in a Grid solution

---
+++ src/2304. Minimum Path Cost in a Grid.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} grid
+ * @param {number[][]} moveCost
+ * @return {number}
+ */
+
+export function minPathCost(grid, moveCost) {
+  const m = grid.length,
+    n = grid[0].length,
+    dp = Array.from(Array(m), () => Array(n).fill(Number.MAX_SAFE_INTEGER))
+
+  for (let i = 0; i < n; i++) {
+    dp[0][i] = grid[0][i]
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      for (let k = 0; k < n; k++) {
+        dp[i][j] = Math.min(
+          dp[i][j],
+          dp[i - 1][k] + moveCost[grid[i - 1][k]][j] + grid[i][j]
+        )
+      }
+    }
+  }
+
+  let min = Number.MAX_SAFE_INTEGER
+
+  for (let i = 0; i < n; i++) {
+    min = Math.min(min, dp[m - 1][i])
+  }
+
+  return min
+}
---
+++ test/2304. Minimum Path Cost in a Grid.test.js
@@ -0,0 +1,39 @@
+import { minPathCost } from '../src/2304. Minimum Path Cost in a Grid'
+
+describe('minPathCost', () => {
+  test('it returns the minimum cost of a path that starts from any cell in the first row and ends at any cell in the last row', () => {
+    expect(
+      minPathCost(
+        [
+          [5, 3],
+          [4, 0],
+          [2, 1],
+        ],
+        [
+          [9, 8],
+          [1, 5],
+          [10, 12],
+          [18, 6],
+          [2, 4],
+          [14, 3],
+        ]
+      )
+    ).toBe(17)
+    expect(
+      minPathCost(
+        [
+          [5, 1, 2],
+          [4, 0, 3],
+        ],
+        [
+          [12, 10, 15],
+          [20, 23, 8],
+          [21, 7, 1],
+          [8, 1, 13],
+          [9, 10, 25],
+          [5, 3, 2],
+        ]
+      )
+    ).toBe(6)
+  })
+})
