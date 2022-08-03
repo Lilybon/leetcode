d13f00859bbfc530bb6603571d44c52c0549c856
Lilybon <zebra10029@gmail.com>
2022-08-03 19:39:39 +0800

    fix: correct 729. My Calendar I assertion

--- test/729. My Calendar I.test.js
+++ test/729. My Calendar I.test.js
@@ -1,7 +1,7 @@
 import { MyCalendar } from '../src/729. My Calendar I'
 
 describe('MyCalendar', () => {
-  test('it finds the largest number that is less than or equal to N with monotone increasing digits', () => {
+  test('it implements the MyCalendar class', () => {
     const myCalendar = new MyCalendar()
     expect(myCalendar.book(10, 20)).toBe(true)
     expect(myCalendar.book(15, 25)).toBe(false)
