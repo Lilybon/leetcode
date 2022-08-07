3ad595473c30715eb6bac6cc2df42194cf4e3515
Lilybon <zebra10029@gmail.com>
2022-08-07 17:43:45 +0800

    refactor: update 2368. Reachable Nodes With Restrictions solution

--- src/2368. Reachable Nodes With Restrictions.js
+++ src/2368. Reachable Nodes With Restrictions.js
@@ -5,26 +5,16 @@
  * @return {number}
  */
 
-class Node {
-  constructor(value) {
-    this.value = value
-    this.siblings = new Set()
-  }
-  addSibling(value) {
-    this.siblings.add(value)
-  }
-}
-
 export function reachableNodes(n, edges, restricted) {
   const map = new Map(),
     restrictedSet = new Set(restricted),
     visited = new Set()
 
   for (let [from, to] of edges) {
-    if (!map.has(from)) map.set(from, new Node(from))
-    if (!map.has(to)) map.set(to, new Node(to))
-    map.get(from).addSibling(to)
-    map.get(to).addSibling(from)
+    if (!map.has(from)) map.set(from, new Set())
+    if (!map.has(to)) map.set(to, new Set())
+    map.get(from).add(to)
+    map.get(to).add(from)
   }
 
   traverse(0)
@@ -32,7 +22,7 @@ export function reachableNodes(n, edges, restricted) {
   function traverse(value) {
     if (visited.has(value) || restrictedSet.has(value)) return
     visited.add(value)
-    for (let sibling of map.get(value).siblings) {
+    for (let sibling of map.get(value)) {
       traverse(sibling)
     }
   }
