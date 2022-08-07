43f0f1f9f576b0a89e50c46bcecb48813f26062e
Lilybon <zebra10029@gmail.com>
2022-08-07 22:01:28 +0800

    feat: add 1743. Restore the Array From Adjacent Pairs solution

---
+++ test/1743. Restore the Array From Adjacent Pairs.test.js
@@ -0,0 +1,20 @@
+import { restoreArray } from '../src/1743. Restore the Array From Adjacent Pairs'
+
+describe('restoreArray', () => {
+  test('it returns the original array nums', () => {
+    expect(
+      restoreArray([
+        [2, 1],
+        [3, 4],
+        [3, 2],
+      ])
+    ).toEqual([1, 2, 3, 4])
+    expect(
+      restoreArray([
+        [4, -2],
+        [1, 4],
+        [-3, 1],
+      ])
+    ).toEqual([-2, 4, 1, -3])
+  })
+})
