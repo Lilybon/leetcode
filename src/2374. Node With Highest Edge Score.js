cf1590937b90cb1b3b88e928d99020f4e6a5cf24
Lilybon <zebra10029@gmail.com>
2022-08-14 14:30:45 +0800

    refactor: update 2374. Node With Highest Edge Score solution

--- src/2374. Node With Highest Edge Score.js
+++ src/2374. Node With Highest Edge Score.js
@@ -2,19 +2,19 @@
  * @param {number[]} edges
  * @return {number}
  */
+
 export function edgeScore(edges) {
-  const map = new Map()
-  for (let i = 0; i < edges.length; i++) {
-    map.set(edges[i], (map.get(edges[i]) ?? 0) + i)
+  const n = edges.length,
+    scores = Array(n).fill(0)
+  for (let i = 0; i < n; i++) {
+    scores[edges[i]] += i
   }
   let result,
     max = -Infinity
-  for (let [node, score] of map) {
-    if (score > max) {
-      max = score
-      result = node
-    } else if (score === max && node < result) {
-      result = node
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] > max) {
+      max = scores[i]
+      result = i
     }
   }
   return result
