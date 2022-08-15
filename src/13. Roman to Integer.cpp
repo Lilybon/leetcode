e7bff5fb7d8361623f864d20cf3c2d97dfab9a5a
Lilybon <zebra10029@gmail.com>
2022-08-15 08:18:40 +0800

    feat: add 13. Roman to Integer solution

---
+++ src/13. Roman to Integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = s.size();
+        unordered_map<char, int> map = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i < n - 1 && map[s[i]] < map[s[i + 1]]) {
+                result -= map[s[i]];
+            } else {
+                result += map[s[i]];
+            }
+        }
+        return result;
+    }
+};
