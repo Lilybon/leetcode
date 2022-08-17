39c65666be0babf28dbde097d4b4ec7eb91d6592
Lilybon <zebra10029@gmail.com>
2022-08-17 20:11:22 +0800

    feat: add 122. Best Time to Buy and Sell Stock II dp solution

--- src/122. Best Time to Buy and Sell Stock II.js
+++ src/122. Best Time to Buy and Sell Stock II.js
@@ -3,6 +3,33 @@
  * @return {number}
  */
 
+// DP
+// function maxProfit(prices) {
+//   const n = prices.length,
+//     dp = Array.from(Array(n), () => [0, 0])
+//   dp[0][1] = -prices[0]
+//   for (let i = 1; i < n; i++) {
+//     dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+//     dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1])
+//   }
+//   return dp[n - 1][0]
+// }
+
+// Optimize DP
+// function maxProfit (prices) {
+//   const n = prices.length
+//   let prevSell = 0,
+//         prevBuy = -prices[0]
+//   for (let i = 1; i < n; i++) {
+//       let currentSell = Math.max(prevSell, prevBuy + prices[i]),
+//           currentBuy = Math.max(prevSell - prices[i], prevBuy)
+//       prevSell = currentSell
+//       prevBuy = currentBuy
+//   }
+//   return prevSell
+// }
+
+// Greedy
 export function maxProfit(prices) {
   let profit = 0
   for (let i = 1; i < prices.length; i++) {
