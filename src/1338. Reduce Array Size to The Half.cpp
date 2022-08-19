a8ba7dc8ab8a00aee62287e22c09812df6160db1
Lilybon <zebra10029@gmail.com>
2022-08-19 19:10:26 +0800

    refactor: update 1338. Reduce Array Size to The Half solution

--- src/1338. Reduce Array Size to The Half.cpp
+++ src/1338. Reduce Array Size to The Half.cpp
@@ -1,20 +1,46 @@
+// Greedy (Array)
+// class Solution {
+//     public:
+//         int minSetSize(vector < int > & arr) {
+//             int n = arr.size();
+//             unordered_map < int, int > map;
+//             for (int x: arr) {
+//                 map[x]++;
+//             }
+//             vector < int > counts;
+//             for (auto & [num, count]: map) {
+//                 counts.push_back(count);
+//             }
+//             sort(counts.rbegin(), counts.rend());
+//             for (int i = 0, count = 0; i < n; ++i) {
+//                 count += counts[i];
+//                 if (count >= n / 2) return i + 1;
+//             }
+//             return counts.size();
+//         }
+// };
+
+// Greedy (Priority Queue)
 class Solution {
     public:
         int minSetSize(vector < int > & arr) {
             int n = arr.size();
             unordered_map < int, int > map;
+            priority_queue < int > priorityQueue;
             for (int x: arr) {
                 map[x]++;
             }
-            vector < int > counts;
             for (auto & [num, count]: map) {
-                counts.push_back(count);
+                priorityQueue.push(count);
             }
-            sort(counts.rbegin(), counts.rend());
-            for (int i = 0, count = 0; i < n; ++i) {
-                count += counts[i];
-                if (count >= n / 2) return i + 1;
+            int count = 0;
+            int result = 0;
+            while (!priorityQueue.empty()) {
+                count += priorityQueue.top();
+                priorityQueue.pop();
+                result++;
+                if (count >= n / 2) return result;
             }
-            return counts.size();
+            return 0;
         }
 };
