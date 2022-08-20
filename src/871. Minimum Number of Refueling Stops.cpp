0e660cec0d3170704d1c507d581e2dd7475d785d
Lilybon <zebra10029@gmail.com>
2022-08-20 21:15:18 +0800

    refactor: update 871. Minimum Number of Refueling Stops solution

--- src/871. Minimum Number of Refueling Stops.cpp
+++ src/871. Minimum Number of Refueling Stops.cpp
@@ -1,21 +1,48 @@
+// DP
+// class Solution {
+//     public:
+//         int minRefuelStops(int target, int startFuel, vector < vector < int >> & stations) {
+//             int n = stations.size();
+//             vector < long > dp(n + 1);
+//             dp[0] = startFuel;
+//             for (int i = 0; i < n; ++i) {
+//                 for (int t = i; t >= 0; --t) {
+//                     if (dp[t] >= stations[i][0]) {
+//                         dp[t + 1] = max(dp[t + 1], dp[t] + stations[i][1]);
+//                     }
+//                 }
+//             }
+//             for (int i = 0; i <= n; ++i) {
+//                 if (dp[i] >= target) {
+//                     return i;
+//                 }
+//             }
+//             return -1;
+//         }
+// };
+
+// Max Heap
 class Solution {
     public:
         int minRefuelStops(int target, int startFuel, vector < vector < int >> & stations) {
             int n = stations.size();
-            vector < long > dp(n + 1);
-            dp[0] = startFuel;
-            for (int i = 0; i < n; ++i) {
-                for (int t = i; t >= 0; --t) {
-                    if (dp[t] >= stations[i][0]) {
-                        dp[t + 1] = max(dp[t + 1], dp[t] + stations[i][1]);
-                    }
+            priority_queue < int > priorityQueue;
+            int curr = startFuel;
+            int i = 0;
+            int result = 0;
+            while (curr < target) {
+                while (i < n && curr >= stations[i][0]) {
+                    priorityQueue.push(stations[i][1]);
+                    i++;
                 }
-            }
-            for (int i = 0; i <= n; ++i) {
-                if (dp[i] >= target) {
-                    return i;
+                if (priorityQueue.empty()) {
+                    return -1;
                 }
+                int dist = priorityQueue.top();
+                priorityQueue.pop();
+                curr += dist;
+                result++;
             }
-            return -1;
+            return result;
         }
 };
