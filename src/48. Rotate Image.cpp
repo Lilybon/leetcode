7c428d7efce154387769e3fd54b6d962f957fd6c
Lilybon <zebra10029@gmail.com>
2022-08-30 08:28:50 +0800

    feat: add 48. Rotate Image solution

---
+++ src/48. Rotate Image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // imagine rotate one by one from outer row to center
+        int n = matrix.size();
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = tmp;
+            }
+        }
+    }
+};
