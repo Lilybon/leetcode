00e34def273c508ba668adf552a2639e7a6ef9d9
Lilybon <zebra10029@gmail.com>
2022-09-04 17:59:13 +0800

    feat: add 2396. Strictly Palindromic Number solution

---
+++ src/2396. Strictly Palindromic Number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+// function isStrictlyPalindromic(n) {
+//   for (let i = 2; i <= n - 2; i++) {
+//     const s = n.toString(i)
+//     for (let j = 0; j <= s.length / 2; j++) {
+//       if (s[j] !== s[s.length - 1 - j]) return false
+//     }
+//   }
+//   return true
+// }
+
+function isStrictlyPalindromic(n) {
+  return false
+}
