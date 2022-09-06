ce57430215f7d15e76074409652c1f668a8b8b75
Lilybon <zebra10029@gmail.com>
2022-09-06 11:34:47 +0800

    refactor: update 122. Best Time to Buy and Sell Stock II solution

--- src/122. Best Time to Buy and Sell Stock II.cpp
+++ src/122. Best Time to Buy and Sell Stock II.cpp
@@ -1,3 +1,36 @@
+// DP
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<array<int, 2>> dp(n, {0, 0});
+//         dp[0][1] = -prices[0];
+//         for (int i = 1; i < n; ++i) {
+//             dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+//             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+//         }
+//         return dp[n - 1][0];
+//     }
+// };
+
+// Optimize DP
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size(),
+//             prevBuy = -prices[0],
+//             prevSell = 0;
+//         for (auto &price : prices) {
+//             int currentBuy = max(prevBuy, prevSell - price),
+//                 currentSell = max(prevSell, prevBuy + price);
+//             prevBuy = currentBuy;
+//             prevSell = currentSell;
+//         }
+//         return prevSell;
+//     }
+// };
+
+// Greedy
 class Solution {
     public:
         int maxProfit(vector < int > & prices) {
