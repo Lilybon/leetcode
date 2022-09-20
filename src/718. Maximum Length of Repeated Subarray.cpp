c787bfbd1248fa8224c620684cf3d5733b428599
Lilybon <zebra10029@gmail.com>
2022-09-20 21:18:26 +0800

    refactor: update 718. Maximum Length of Repeated Subarray solution

--- src/718. Maximum Length of Repeated Subarray.cpp
+++ src/718. Maximum Length of Repeated Subarray.cpp
@@ -1,16 +1,40 @@
+// DP
+// class Solution {
+// public:
+//     int findLength(vector<int>& nums1, vector<int>& nums2) {
+//         int m = nums1.size(), n = nums2.size(), ans = 0;
+//         vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+//         for (int i = 1; i <= m; ++i) {
+//             for (int j = 1; j <= n; ++j) {
+//                 if (nums1[i - 1] == nums2[j - 1])
+//                     dp[i][j] = dp[i - 1][j - 1] + 1;
+//                 else
+//                     dp[i][j] = 0;
+//                 ans = max(ans, dp[i][j]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// Optimized DP
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1));
-        int maxLength = 0;
-        for (int i = nums1.size() - 1; i >= 0; --i) {
-            for (int j = nums2.size() - 1; j >= 0; --j) {
-                if (nums1[i] == nums2[j]) {
-                    dp[i][j] = dp[i + 1][j + 1] + 1;
-                    maxLength = max(maxLength, dp[i][j]);
-                }
+        if (nums1.size() < nums2.size()) return findLength(nums2, nums1);
+        int m = nums1.size(), n = nums2.size();
+        vector<int> current(n + 1), prev(n + 1);
+        int ans = 0;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (nums1[i - 1] == nums2[j - 1])
+                    current[j] = prev[j - 1] + 1;
+                else
+                    current[j] = 0;
+                ans = max(ans, current[j]);
             }
+            current.swap(prev);
         }
-        return maxLength;
+        return ans;
     }
 };
--- src/718. Maximum Length of Repeated Subarray.py
+++ src/718. Maximum Length of Repeated Subarray.py
@@ -1,8 +1,30 @@
+# DP
+# class Solution:
+#     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+#         m, n, ans = len(nums1), len(nums2), 0
+#         dp = [[0] * (n + 1) for _ in range(m + 1)]
+#         for i in range(1, m + 1):
+#             for j in range(1, n + 1):
+#                 if nums1[i - 1] == nums2[j - 1]:
+#                     dp[i][j] = dp[i - 1][j - 1] + 1
+#                 else
+#                     dp[i][j] = 0
+#                     ans = max(ans, dp[i][j])
+#         return ans
+
+# Optimized DP
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
-        for i in range(len(nums1) - 1, -1, -1):
-            for j in range(len(nums2) - 1, -1, -1):
-                if nums1[i] == nums2[j]:
-                    dp[i][j] = dp[i + 1][j + 1] + 1
-        return max(max(row) for row in dp)
+        if len(nums1) < len(nums2):
+            return self.findLength(nums2, nums1)
+        m, n, ans = len(nums1), len(nums2), 0
+        current, prev = [0] * (n + 1), [0] * (n + 1)
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if nums1[i - 1] == nums2[j - 1]:
+                    current[j] = prev[j - 1] + 1
+                else:
+                    current[j] = 0
+                ans = max(ans, current[j])
+            current, prev = prev, current
+        return ans
