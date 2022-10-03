504ea39f336dfd8d25ff8eaaa2c944f696dcbeae
Lilybon <zebra10029@gmail.com>
2022-10-03 16:43:37 +0800

    feat: add 2428. Maximum Sum of an Hourglass solution

---
+++ src/2428. Maximum Sum of an Hourglass.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        ans = 0
+        for i in range(m - 2):
+            for j in range(n - 2):
+                ans = max(
+                    ans,
+                    grid[i][j] +
+                    grid[i][j + 1] +
+                    grid[i][j + 2] +
+                    grid[i + 1][j + 1] +
+                    grid[i + 2][j] +
+                    grid[i + 2][j + 1] +
+                    grid[i + 2][j + 2]
+                )
+        return ans
