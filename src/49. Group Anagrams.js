1ef2bec64f892c48b5ce1f6cabd3281b93b7180c
Lilybon <zebra10029@gmail.com>
2022-10-28 18:52:06 +0800

    refactor: update 49. Group Anagrams solution

--- src/49. Group Anagrams.js
+++ src/49. Group Anagrams.js
@@ -6,9 +6,13 @@
 export function groupAnagrams(strs) {
   const dict = new Map()
   strs.forEach((str) => {
-    const key = str.split('').sort().join()
+    const counts = Array(26).fill(0)
+    for (let c of str) {
+      counts[c.charCodeAt(0) - 97]++
+    }
+    const key = counts.join('#')
     if (!dict.get(key)) dict.set(key, [])
-    dict.set(key, [...dict.get(key), str])
+    dict.get(key).push(str)
   })
   return [...dict.values()]
 }
