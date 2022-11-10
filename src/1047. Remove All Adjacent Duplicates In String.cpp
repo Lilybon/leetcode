3b2e6b1986326e12293aac75fc76796063e36e83
Lilybon <zebra10029@gmail.com>
2022-11-10 10:56:50 +0800

    feat: add 1047. Remove All Adjacent Duplicates In String solution

---
+++ src/1047. Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans;
+        for (char c : s) {
+            if (ans.size() && ans.back() == c) {
+                ans.pop_back(); 
+            } else {
+                ans.push_back(c); 
+            }
+        }
+        return ans;
+    }
+};
