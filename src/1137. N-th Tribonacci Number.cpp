a47d31e1108b52075426569c5546f191a3fae74d
Lilybon <zebra10029@gmail.com>
2023-01-30 22:34:11 +0800

    refactor: update 1137. N-th Tribonacci Number solution

--- src/1137. N-th Tribonacci Number.cpp
+++ src/1137. N-th Tribonacci Number.cpp
@@ -1,14 +1,29 @@
+// class Solution {
+// public:
+//     int tribonacci(int n) {
+//         if (n <= 1) return n;
+//         if (n == 2) return 1;
+//         vector<int> dp(n + 1);
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         dp[2] = 1;
+//         for (int i = 3; i <= n; ++i)
+//             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+//         return dp.back();
+//     }
+// };
+
 class Solution {
 public:
     int tribonacci(int n) {
-        if (n <= 1) return n;
-        if (n == 2) return 1;
-        vector<int> dp(n + 1);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-        for (int i = 3; i <= n; ++i)
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-        return dp.back();
+        int dp[3] = {0, 1, 1};
+        if (n <= 2) return dp[n];
+        for (int i = 3; i <= n; ++i) {
+            int nextDp1 = dp[2];
+            dp[2] = dp[0] + dp[1] + dp[2];
+            dp[0] = dp[1];
+            dp[1] = nextDp1;
+        }
+        return dp[2];
     }
 };
