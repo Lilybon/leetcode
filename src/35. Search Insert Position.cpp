c41d46448d6a54d8aa82dc703210f2e8c0f72e43
Lilybon <zebra10029@gmail.com>
2023-02-20 11:03:15 +0800

    refactor: update 35. Search Insert Position solution

--- src/35. Search Insert Position.cpp
+++ src/35. Search Insert Position.cpp
@@ -1,3 +1,10 @@
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+//     }
+// };
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
