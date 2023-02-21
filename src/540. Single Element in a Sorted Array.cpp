781fb5967e0138bb4026fac27a8b5696006726a5
Lilybon <zebra10029@gmail.com>
2023-02-21 12:45:39 +0800

    refactor: update 540. Single Element in a Sorted Array solution

---
+++ src/540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (mid % 2 == 0 && nums[mid] == nums[mid + 1]) {
+                left = mid + 1;
+            } else if (mid % 2 == 0 && nums[mid] != nums[mid + 1]) {
+                right = mid - 1;
+            } else if (mid % 2 == 1 && nums[mid] == nums[mid - 1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return nums[left];
+    }
+};
--- src/540. Single Element in a Sorted Array.js
+++ src/540. Single Element in a Sorted Array.js
@@ -4,11 +4,36 @@
  */
 
 export function singleNonDuplicate(nums) {
+  if (nums.length === 1) {
+    return nums[0]
+  }
+
+  let left = 0,
+    right = nums.length - 1
+  while (left <= right) {
+    const mid = Math.floor((right + left) / 2)
+    if (mid % 2 === 0 && nums[mid] === nums[mid + 1]) {
+      left = mid + 1
+    } else if (mid % 2 === 0 && nums[mid] !== nums[mid + 1]) {
+      right = mid - 1
+    } else if (mid % 2 === 1 && nums[mid] === nums[mid - 1]) {
+      left = mid + 1
+    } else {
+      right = mid - 1
+    }
+  }
+
+  return nums[left]
+}
+
+/*
+export function singleNonDuplicate(nums) {
   // because of it is 'sorted' array, we don't need to xor all the array.
   for (let i = 0; i < nums.length; i += 2) {
     if (nums[i] !== nums[i + 1]) return nums[i]
   }
 }
+ */
 
 /*
 export function singleNonDuplicate (nums) {
