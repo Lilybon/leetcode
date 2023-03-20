ed1eb9b50c8f89936d1e53f6484a8e237be405b5
Lilybon <zebra10029@gmail.com>
2023-03-20 14:42:56 +0800

    feat: add 605. Can Place Flowers solution

---
+++ src/605. Can Place Flowers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        for (int i = 0; i < flowerbed.size() && n > 0; ++i) {
+            if (
+                (i == 0 || flowerbed[i - 1] == 0) &&
+                flowerbed[i] == 0 &&
+                (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)
+            ) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n == 0;
+    }
+};
