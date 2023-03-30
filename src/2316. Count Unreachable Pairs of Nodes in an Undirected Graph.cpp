fcddab014fd8e96acb7775eae549f72eb1c38d85
Lilybon <zebra10029@gmail.com>
2023-03-30 10:03:05 +0800

    refactor: update 2316. Count Unreachable Pairs of Nodes in an Undirected Graph solution

--- src/2316. Count Unreachable Pairs of Nodes in an Undirected Graph.cpp
+++ src/2316. Count Unreachable Pairs of Nodes in an Undirected Graph.cpp
@@ -28,21 +28,16 @@ public:
             union_(edge[0], edge[1]);
         }
         
-        vector<long long> prefixSums(n, 0);
+        vector<long long> nodeCounts(n, 0);
         for (int i = 0; i < n; ++i) {
-            prefixSums[find(uf[i])]++;
+            nodeCounts[find(uf[i])]++;
         }
-        for (int i = 1; i < n; ++i) {
-            prefixSums[i] += prefixSums[i - 1];
+        long long totalEdgeCount = 0;
+        int totalNodeCount = 0;
+        for (auto nodeCount : nodeCounts) {
+            totalNodeCount += nodeCount;
+            totalEdgeCount += nodeCount * (n - totalNodeCount);
         }
-
-        long long unreachableCount = 0;
-        int beforeSum = 0;
-        for (int i = 0; i < n; ++i) {
-            int currentSum = prefixSums[i] - beforeSum;
-            unreachableCount += currentSum * (prefixSums[n - 1] - prefixSums[i]);
-            beforeSum += currentSum;
-        }
-        return unreachableCount;
+        return totalEdgeCount;
     }
 };
