78c2c675fa4b4216bb26611885af862caaae6552
Lilybon <zebra10029@gmail.com>
2023-05-11 19:52:29 +0800

    feat: add 1035. Uncrossed Lines solution

---
+++ src/1035. Uncrossed Lines.cpp
@@ -0,0 +1,36 @@
+// class Solution {
+// public:
+//     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+//         const int n1 = nums1.size(), n2 = nums2.size();
+//         vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+//         for (int i = 1; i <= n1; ++i) {
+//             for (int j = 1; j <= n2; ++j) {
+//                 if (nums1[i - 1] == nums2[j - 1]) {
+//                     dp[i][j] = dp[i - 1][j - 1] + 1;
+//                 } else {
+//                     dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+//                 }
+//             }
+//         }
+//         return dp[n1][n2];
+//     }
+// };
+
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        const int n1 = nums1.size(), n2 = nums2.size();
+        vector<int> dp(n2 + 1), dpPrev(n2 + 1);
+        for (int i = 1; i <= n1; ++i) {
+            for (int j = 1; j <= n2; ++j) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[j] = dpPrev[j - 1] + 1;
+                } else {
+                    dp[j] = max(dp[j - 1], dpPrev[j]);
+                }
+            }
+            dpPrev = dp;
+        }
+        return dp[n2];
+    }
+};
