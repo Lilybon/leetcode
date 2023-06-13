237c0db42fc43ac65e559195997e4e094e7994c7
Lilybon <zebra10029@gmail.com>
2023-06-13 20:09:04 +0800

    fix: correct 1351. Count Negative Numbers in a Sorted Matrix solution

--- src/1351. Count Negative Numbers in a Sorted Matrix.cpp
+++ src/1351. Count Negative Numbers in a Sorted Matrix.cpp
@@ -4,8 +4,9 @@ public:
         const int m = grid.size(), n = grid[0].size();
         int count = 0, j = n - 1;
         for (int i = 0; i < m; ++i) {
-            int left = -1, right = j;
-            lower_bound();
+            while (j >= 0 && grid[i][j] < 0) {
+                j--;
+            }
             count += n - 1 - j;
         }
         return count;
