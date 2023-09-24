5337578c091e28b605b8505c0bc7223cbbfc49ca
Lilybon <zebra10029@gmail.com>
2023-09-24 19:32:11 +0800

    feat: add 1337. The K Weakest Rows in a Matrix solution

---
+++ src/1337. The K Weakest Rows in a Matrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        def binarySearch(arr: List[int]) -> int:
+            left, right = 0, len(arr) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if arr[mid] == 1:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left
+
+        queue = [(binarySearch(row), idx) for idx, row in enumerate(mat)]
+        heapq.heapify(queue)
+
+        return [idx for _, idx in heapq.nsmallest(k, queue)]
